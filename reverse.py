a811e47c8cb9f4061b37a8bc3da9a07281adc943
John Kennedy <devjckennedy@gmail.com>
2017-04-04 10:21:55 +0100

    Refined the Reverse function

--- reverse.py
+++ reverse.py
@@ -1,9 +1,3 @@
 def reverse(value):
     "Reverses an iterable sequence or returns None"
-    if (type(value) == str):
-        valLen = len(value)
-        return value[valLen:-valLen - 1:-1]
-    elif (type(value) == list or type(value) == tuple):
-        return value[::-1]
-    else:
-        return "Cannot reverse argument."
+    return value[::-1]
